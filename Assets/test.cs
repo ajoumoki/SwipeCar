f93a43a00fbddad3da62b28b35bae4de8ad8a939
R.M. <bda07-s250215@gclass.ice.go.kr>
2026-05-13 14:43:59 +0900

    Update test.cs

--- Assets/test.cs
+++ Assets/test.cs
@@ -6,19 +6,19 @@ using UnityEngine;
 public class test : MonoBehaviour
 {
     // Start is called before the first frame update
-    /* GameObject flag;
+    /* GameObject flag; //게임 오브젝트 flag
     void Start()
     {
-        flag = GameObject.Find("flag");
+        flag = GameObject.Find("flag"); // 게임 오브젝트 flag 찾기
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) //마우스 왼쪽 버튼을 눌렀으면
         {
-            transform.position = flag.transform.position;
+            transform.position = flag.transform.position; //자동차 위치 깃발 위치로 변경
         }
     } */
 }
